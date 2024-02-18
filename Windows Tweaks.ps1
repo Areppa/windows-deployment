36f54f06bd2025a86cfaabf3e777ed3e3917df7b
Aaro Halme <aaro.halme2000@gmail.com>
2024-02-18 07:51:30 +0200

    Added new feature that removes online results from start menu search

--- Windows Tweaks.ps1
+++ Windows Tweaks.ps1
@@ -106,6 +106,15 @@ Write-Output "Disabling Error reporting..."
 	Disable-ScheduledTask -TaskName "Microsoft\Windows\Windows Error Reporting\QueueReporting" | Out-Null
 
 
+################################################################ REMOVE ONLINE RESULTS FROM SEARCH ############################################################
+Write-Output "Removing online results from search..."
+	If (!(Test-Path 'HKCU:\\SOFTWARE\\Policies\\Microsoft\\Windows\\Explorer')) {
+		New-Item -Path 'HKCU:\\SOFTWARE\\Policies\\Microsoft\\Windows\\Explorer' -Force | Out-Null
+	}
+	New-ItemProperty -Path 'HKCU:\\SOFTWARE\\Policies\\Microsoft\\Windows\\Explorer' -Name 'DisableSearchBoxSuggestions' -Type DWord -Value 1 -Force
+	Stop-Process -name explorer -force
+
+
 ###############################################################################################################################################################
 #                                                                                                                                                             #
 #                                                                       SERVICE TWEAKS                                                                        #
